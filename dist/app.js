2f25dddc4fbfca1e09db3052b034aa387f71228a
Talhaa-129 <talhakhan5023@gmail.com>
2022-09-25 21:50:05 +0500

    six commit

--- dist/app.js
+++ dist/app.js
@@ -10,8 +10,6 @@ const typeorm_1 = require("typeorm");
 const createRoutes_1 = require("./Routes/createRoutes");
 const Client_1 = require("./Entities/Client");
 const Blogs_1 = require("./Entities/Blogs");
-const https_1 = __importDefault(require("https"));
-const fs_1 = __importDefault(require("fs"));
 const PORT = process.env.PORT || 5000;
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
@@ -19,7 +17,7 @@ app.use((0, cors_1.default)());
 app.use("/api", createRoutes_1.ClientRouter);
 const Appdatasource = new typeorm_1.DataSource({
     type: "postgres",
-    host: "44.201.130.220",
+    host: "44.204.88.193",
     port: 5432,
     username: "postgres",
     password: "password",
@@ -32,26 +30,10 @@ const Appdatasource = new typeorm_1.DataSource({
 Appdatasource.initialize()
     .then(() => {
     console.log("Database Connected Successfully...");
-    https_1.default
-        .createServer({
-        key: fs_1.default.readFileSync("attech.pem"),
-        cert: fs_1.default.readFileSync("cert.pem"),
-    }, app)
-        .listen(PORT, () => {
+    app.listen(PORT, () => {
         console.log("Successfully Run on Port 5000.....");
     });
 })
     .catch((err) => {
     console.error("Error Database Connected");
 });
-// https
-// .createServer(
-//   {
-//     key: fs.readFileSync("attech.pem"),
-//     cert: fs.readFileSync("cert.pem"),
-//   },
-//   app
-// )
-// .listen(5000, () => {
-//   console.log("Successfully Run on Port 5000.....");
-// });
